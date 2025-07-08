920253cd94868be5984c3ba29d87801860003f51
mxjeczka <maja.perz15@gmail.com>
2025-07-08 12:52:07 +0200

    change2

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 const sentences = [
-    `<p>Wszystkiego najlepszego z okazji</p>`,
-    `<p class="rainbow-text">
-        <span>u</span><span>r</span><span>o</span><span>d</span><span>z</span><span>i</span><span>n</span>
-    </p>`
+    `<p>Wszystkiego najlepszego z okazji</p>`
 ];
 
+const rainbowHTML = `
+    <span>u</span><span>r</span><span>o</span><span>d</span><span>z</span><span>i</span><span>n</span>
+`;
+
 const textWrapper = document.getElementById('text-wrapper');
+const rainbowWrapper = document.getElementById('rainbow-wrapper');
 const container = document.getElementById('click-container');
 const catBalloonGif = document.getElementById('catBalloon-gif');
 
 let visibleCount = 0;
 
 container.addEventListener('click', () => {
-    if (visibleCount < sentences.length) {
+    if (visibleCount === 0) {
         const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = sentences[visibleCount];
+        tempDiv.innerHTML = sentences[0];
         const p = tempDiv.firstElementChild;
-
         textWrapper.appendChild(p);
-
         setTimeout(() => {
             p.classList.add('visible');
         }, 10);
-
-        // Wenn der zweite Satz angezeigt wird, dann GIF sichtbar machen
-        if (visibleCount === 1) {
-            catBalloonGif.classList.add('visible');
-        }
-
+        visibleCount++;
+    } else if (visibleCount === 1) {
+        rainbowWrapper.innerHTML = rainbowHTML;
+        rainbowWrapper.classList.add('visible');
+        catBalloonGif.classList.add('visible');
         visibleCount++;
     } else {
         window.location.href = 'pictures.html';
