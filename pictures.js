f343ebe7246c413a70cc964bf2d0d30395683be9
mxjeczka <maja.perz15@gmail.com>
2025-07-09 16:39:15 +0200

    change28

--- pictures.js
+++ pictures.js
@@ -12,7 +12,22 @@ const images = [
     'assets/images/image1.jpg',
     'assets/images/image2.jpg',
     'assets/images/image3.jpg',
-    'assets/images/image4.jpg'
+    'assets/images/image4.jpg',
+    'assets/images/image5.jpg',
+    'assets/images/image6.jpg',
+    'assets/images/image7.jpg',
+    'assets/images/image8.jpg',
+    'assets/images/image9.jpg',
+    'assets/images/image10.jpg',
+    'assets/images/image11.jpg',
+    'assets/images/image12.jpg',
+    'assets/images/image13.jpg',
+    'assets/images/image14.jpg',
+    'assets/images/image15.jpg',
+    'assets/images/image16.jpg',
+    'assets/images/image17.jpg',
+    'assets/images/image18.jpg',
+    'assets/images/image19.jpg'
 ];
 
 const container = document.getElementById('image-container');
